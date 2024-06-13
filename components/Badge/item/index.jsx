70061aa0c5dd20f2eb80e55dade28c9db4d21dc9
Justin <ju3tin@hotmail.co.uk>
2024-06-13 01:26:45 +0100

    dfg

---
+++ components/Badge/item/index.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+function JSONComponent({ jsonData }) {
+  return (
+    <div className='grid grid-4-4-4 centered'>
+      {jsonData && jsonData.map((item, index) => (
+        <JSONItem key={index} data={item} />
+      ))}
+    </div>
+  );
+}
+
+function JSONItem({ data }) {
+  return (
+<div className="badge-item-stat">
+        {/* TEXT STICKER */}
+        <p className="text-sticker">
+          {/* TEXT STICKER ICON */}
+          <svg className="text-sticker-icon icon-plus-small">
+            <use xlinkHref="#svg-plus-small"></use>
+          </svg>
+          {/* TEXT STICKER ICON */}
+          {data.number} Exp
+        </p>
+        {/* /TEXT STICKER */}
+    
+        {/* BADGE ITEM STAT IMAGE PREVIEW */}
+        <img className="badge-item-stat-image-preview" src={data.image} alt={`badge-${data.lilname}-s`} />
+        {/* /BADGE ITEM STAT IMAGE PREVIEW */}
+    
+        {/* BADGE ITEM STAT IMAGE */}
+        <img className="badge-item-stat-image" src={`/img/badge/${data.lilname}-b.png`} alt={`badge-${data.lilname}-b`} />
+        {/* /BADGE ITEM STAT IMAGE */}
+    
+        {/* BADGE ITEM STAT TITLE */}
+        <p className="badge-item-stat-title">Bronze User</p>
+        {/* /BADGE ITEM STAT TITLE */}
+    
+        {/* BADGE ITEM STAT TEXT */}
+        <p className="badge-item-stat-text">Has posted more than 1 post on their profile</p>
+        {/* /BADGE ITEM STAT TEXT */}
+    
+        {/* PROGRESS STAT */}
+        <div className="progress-stat">
+          {/* PROGRESS STAT BAR */}
+          <div id={`badge-${data.lilname}`} className="progress-stat-bar" style={{'width': '204px', 'height': '4px', 'position': 'relative'}}><canvas width="204" height="4" style={{'position': 'absolute', 'top': '0px', 'left': '0px'}}></canvas><canvas width="204" height="4" style={{'position': 'absolute', 'top': '0px', 'left': '0px'}}></canvas></div>
+          {/* /PROGRESS STAT BAR */}
+    
+          {/* BAR PROGRESS WRAP */}
+          <div className="bar-progress-wrap">
+            {/* BAR PROGRESS INFO */}
+            <p className="bar-progress-info negative center">unlocked!</p>
+            {/* /BAR PROGRESS INFO */}
+          </div>
+          {/* /BAR PROGRESS WRAP */}
+        </div>
+        {/* /PROGRESS STAT */}
+      </div>);}
+export default JSONComponent;
---
+++ pages/badge/index.js
@@ -0,0 +1,68 @@
+
+import React, { useEffect } from 'react';
+import Navcomponent from '../../components/Navigation/navwidget1';
+import Navcomponent1 from '../../components/Navigation/navwidget2';
+import Navcomponent2 from '../../components/Navigation/navwidget3';
+import Chatwidget from '../../components/Chat/chatwidget';
+import Chatwidget1 from '../../components/Chat/chatwidget1';
+import BadgeItems from '../../components/Badge/item';
+import Header1 from '../../components/Header';
+import Bar1 from '../../components/Bar';
+import Banner from '../../components/Banner';
+import Head from 'next/head';
+import Groupmenu from '../../components/Group/menu';
+import jsonData from '../../groupstest.json';
+import jsonData2 from '../../menuitems.json';
+import jsonData3 from '../../badges.json';
+import Memberitem from '../../components/Member/item';
+
+function Badges() {
+    const bannerImageUrl = '/img/banner/badges-icon.png';
+    const title1 = 'Badges';
+    const description = 'Check out all your unlocked and locked badges!';
+
+ useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "/js/app.bundle.min.js";
+    script.async = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+     }; 
+  }, []);
+  return (
+    
+    <div>
+     <Head>
+        <title>{title1}</title>
+
+        <link rel="stylesheet" href="/css/vendor/bootstrap.min.css" />
+  <link rel="stylesheet" href="/css/styles.min.css" />
+  <link rel="stylesheet" href="/css/vendor/simplebar.css" />
+ 
+  <link rel="stylesheet" href="/css/vendor/tiny-slider.css" />
+
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+    <Navcomponent  jsonData={jsonData2}  />
+    <Navcomponent1 />
+    <Navcomponent2 />
+    <Chatwidget />
+    <Chatwidget1 />
+    <Header1 />
+    <Bar1 />
+    <div className="content-grid" style={{'transform': 'translate(368px)', 'transition': 'transform 0.4s ease-in-out'}}>
+   <Banner bannerdescription={description} bannertitle={title1} bannerimage={bannerImageUrl}/>
+   <BadgeItems jsonData={jsonData3} />
+
+      {/* /GRID COLUMN */}
+    </div>
+   
+
+  </div>
+   
+    
+  );
+}
+
+export default Badges;
